f2ea7bcb0640dc08c950321cdc6b1d26659f21f3
User8642 <76618149+User8642@users.noreply.github.com>
2020-12-27 15:28:39 +0300

    Update задание номер 2.py

--- задание номер 2.py
+++ задание номер 2.py
@@ -2,5 +2,11 @@ print("Hello user!")
 
 user_name = input("what is your name?  ")
 
-print('Hello', user_name )
+print('Hello' , user_name )
+
+user_year = int(input("When you was born? "))
+
+print("ooh my Godness,", user_year)
+
+print(2020 - user_year)
 
