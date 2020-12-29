aad7b12d8e0aafe3faabe0392bda6648e1b6cb95
User8642 <76618149+User8642@users.noreply.github.com>
2020-12-29 18:24:32 +0300

    Create utythfnjh gfhjktq.py

---
+++ utythfnjh gfhjktq.py
@@ -0,0 +1,7 @@
+import string
+import random
+def randompassword():
+    chars=string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase
+    size= 8
+    return '' . join(randompassword(chars))
+print(randompassword)
